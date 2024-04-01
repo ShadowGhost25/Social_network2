687d9c2da4f591bf3d7e393dd23e2916dc4472d5
ShadowGhost25 <ilyushavavi@gmail.com>
2024-04-01 12:32:08 +0300

    test

--- my-app/src/pages/Message/Message.jsx
+++ my-app/src/pages/Message/Message.jsx
@@ -1,40 +1,20 @@
 import Header from "../../componets/Header/Header";
 import s from "./message.module.css";
 import ava from "./img/ava.png";
+import { useDispatch } from "react-redux";
+import React from "react";
+import { fetchAuthMe } from "../../redux/slices/login";
 
 const Message = () => {
+
+
     return (
         <>
             <Header />
-            <div className={s.mainMessage}>
-                <div className={s.mainFriends}>
-                    <div className={s.headerMessage}></div>
-                    <div className={s.foterMessage}>
-                        <div className={s.img}>
-                            <img className={s} src={ava} alt="no img" />
-                        </div>
-                        <div className={s.textMessage}>
-                            <h3 className={s.h3}>Саша Тарасов</h3>
-                            <span>Вы: Прикольно</span>
-                        </div>
-                            <span>15:23</span>
-                    </div>
-                </div>
-            </div>
-            <div className={s.mainMessage}>
-                <div className={s.mainFriends}>
-                    <div className={s.headerMessage}></div>
-                    <div className={s.foterMessage}>
-                        <div className={s.img}>
-                            <img className={s} src={ava} alt="no img" />
-                        </div>
-                        <div className={s.textMessage}>
-                            <h3 className={s.h3}>Саша Тарасов</h3>
-                            <span>Вы: Прикольно</span>
-                        </div>
-                            <span>15:23</span>
-                    </div>
-                </div>
+            <div className={s.main}>
+              <div className={s.mainMessage}>
+                re
+              </div>
             </div>
         </>
     );
