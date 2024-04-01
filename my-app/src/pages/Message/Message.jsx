1e4447de8bfb2ff6a4190f0d45ce6811c9979f40
ShadowGhost25 <ilyushavavi@gmail.com>
2024-04-01 14:52:08 +0300

    for changes

--- my-app/src/pages/Message/Message.jsx
+++ my-app/src/pages/Message/Message.jsx
@@ -1,23 +1,34 @@
+import React from "react";
 import Header from "../../componets/Header/Header";
 import s from "./message.module.css";
 import ava from "./img/ava.png";
-import { useDispatch } from "react-redux";
-import React from "react";
-import { fetchAuthMe } from "../../redux/slices/login";
+import Search from "../../componets/Search/Search";
+import FriendsOnline from "../../componets/FriendsOnline/FriendsOnline";
 
 const Message = () => {
-
-
-    return (
-        <>
-            <Header />
-            <div className={s.main}>
-              <div className={s.mainMessage}>
-                re
+  return (
+    <>
+      <Header />
+      <div className={s.main}>
+        <div className={s.mainMessage}>
+          <div className={s.userFriends}>
+            <Search />
+            <div className={s.positionBlock}>
+              <img src={ava} alt="no img" />
+              <div className={s.positionBlockName}>
+                <span className={s.nameFriends}>Илья Вавилин</span>
+                <div className={s.positionBlockStatus}>
+                  <span className={s.statusFriends}> Online </span>
+                  <div className={s.ellips}></div>
+                </div>
               </div>
             </div>
-        </>
-    );
-}
+          </div>
+          <div className={s.messageFriends}></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Message;
