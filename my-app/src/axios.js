17be51001c4bdae971e6372892d9d53e2f992fbe
Ilyusha <ilyushavavi@gmail.com>
2024-05-30 22:58:50 +0300

    fix json

--- my-app/src/axios.js
+++ my-app/src/axios.js
@@ -3,7 +3,7 @@ import { localHost } from "./Route/route";
 const instance = axios.create({
   baseURL: localHost,
 });
-
+//    // "start": "node --max-old-space-size=2048 node_modules/.bin/react-scripts start",
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem("token");
   return config
